39120663b6a7f560d70fc30ea19014f54ea2ef58
Rafał Chojnacki <rafalchojnacki0502@gmail.com>
2022-03-06 13:46:21 +0100

    added setter

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Python training project
 
 # Run
 `python3 main.py`
+
+
+
+
+# Description
+
+`@property` changes the behaviour of the function in such a way, that we need to 
+call the function, we can refer to it
--- main.py
+++ main.py
@@ -3,12 +3,24 @@ class Driver:
     def __init__(self,name):
         self._name = name       # python convention -> private
     
+    
+    @property
     def name(self):             # getter function
         return self._name
     
     
-
+    @name.setter                # @name refers to def name(self) function
+    def name(self, name):
+        self._name = name
+    
 
 if __name__ == "__main__":
     dd = Driver('Rick Falugia')
-    print(dd.name())
+    # print(dd.name())
+    
+    # after added property decorator
+    print(dd.name)
+    
+    # after added setter
+    dd.name = "Ricky Martin"
+    print(dd.name)
